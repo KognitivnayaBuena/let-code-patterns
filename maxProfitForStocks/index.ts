b07cabd3971a5b41c9b8742ce2510cc876969890
Алена <mesherla001@gmail.com>
2022-01-25 17:39:58 +0100

    day 6 - maxProfit for stocks solution

--- maxProfitForStocks/index.ts
+++ maxProfitForStocks/index.ts
@@ -1,1 +1,14 @@
-function maxProfit(prices: number[]): number {}
+// O(n) solution
+function maxProfit(prices: number[]): number {
+  let maxProfit = 0;
+  let curMin = prices[0];
+
+  for (let i = 0; i < prices.length; i++) {
+    let price = prices[i];
+
+    maxProfit = Math.max(maxProfit, price - curMin);
+    curMin = Math.min(curMin, price);
+  }
+
+  return maxProfit;
+}
