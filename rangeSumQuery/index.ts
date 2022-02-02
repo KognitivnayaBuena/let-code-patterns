c817afe4fcf505af7baee2129c46d748b2a875d2
Алена <mesherla001@gmail.com>
2022-02-02 12:07:12 +0100

    day 9 - add new solution for range sum query

--- rangeSumQuery/index.ts
+++ rangeSumQuery/index.ts
@@ -22,3 +22,30 @@ const RIGHT = 2;
 
 let obj = new NumArray(NUMS);
 let sum = obj.sumRange(LEFT, RIGHT); // 6
+
+// faster solution, one sum running O(n) and get result for O(1)
+// Take memory for sums result O(n)
+class NumArrayV2 {
+  nums: number[];
+  sums: number[];
+  currentSum: number;
+  constructor(nums: number[]) {
+    this.nums = nums;
+    this.currentSum = 0;
+    this.sums = [];
+
+    for (let i = 0; i < nums.length; i++) {
+      this.currentSum += nums[i];
+
+      this.sums.push(this.currentSum);
+    }
+  }
+
+  sumRange(left: number, right: number): number {
+    if (left === 0) {
+      return this.sums[right];
+    }
+
+    return this.sums[right] - this.sums[left - 1];
+  }
+}
